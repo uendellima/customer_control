d333904559fe54db856c195e13d77bea2b306510
Uendel Lima <uendel@polisystem.com.br>
2025-10-20 12:51:37 -0300

    feat: add CRUD operations for Client and Contact routes

--- backend/src/routes/ClientRoutes.js
+++ backend/src/routes/ClientRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createClient, listClients } from "../controllers/ClientController.js";
+import {
+  createClient,
+  listClients,
+  getClientById,
+  updateClient,
+  deleteClient,
+} from "../controllers/ClientController.js";
 
 const router = express.Router();
 
 router.post("/clients", createClient);
 router.get("/clients", listClients);
+router.get("/clients/:id", getClientById);
+router.put("/clients/:id", updateClient);
+router.delete("/clients/:id", deleteClient);
 
 export default router;
--- backend/src/routes/ContactRoutes.js
+++ backend/src/routes/ContactRoutes.js
@@ -2,11 +2,17 @@ import express from "express";
 import {
   createContact,
   listContacts,
+  deleteContact,
+  updateContact,
+  getContactById,
 } from "../controllers/ContactController.js";
 
 const router = express.Router();
 
 router.post("/contacts", createContact);
 router.get("/contacts/:clientId", listContacts);
+router.delete("/contacts/:id", deleteContact);
+router.put("/contacts/:id", updateContact);
+router.get("/contacts/:id", getContactById);
 
 export default router;
